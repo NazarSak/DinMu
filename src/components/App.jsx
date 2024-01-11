4792b01135be4dc88dbaa6e8e242fbdb0bfa0d96
NazarSak <112251832+NazarSak@users.noreply.github.com>
2024-01-11 21:57:26 +0200

    prepare project to develop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
+import React from "react";
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <section>
+       
+    </section>
+      <h2>asdasd</h2>
+    </>
   );
 };
+
+
+export default App;
